a909b38d8af48d91d9832130f4f976982ec433ba
Hentry Martin <hentrymartin.p@gmail.com>
2026-03-18 14:49:06 +0100

    added back tc member auth0 jwts

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -3,10 +3,10 @@ import { CompositeAuth } from '@mastra/core/server';
 
 export const apiAuthLayer = new CompositeAuth([
   // // TC Member Auth0 JWTs
-  // new MastraAuthAuth0({
-  //   domain: process.env.AUTH0_DOMAIN,
-  //   audience: process.env.AUTH0_AUDIENCE,
-  // }),
+  new MastraAuthAuth0({
+    domain: process.env.AUTH0_DOMAIN,
+    audience: process.env.AUTH0_AUDIENCE,
+  }),
   // TC M2M Auth0 JWTs
   new MastraAuthAuth0({
     domain: process.env.AUTH0_M2M_DOMAIN ?? 'topcoder-dev.auth0.com',
