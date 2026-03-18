4a10edfd7e7b78db00ef97ef546489c5ed1804a9
Hentry Martin <hentrymartin.p@gmail.com>
2026-03-18 03:18:30 +0100

    commenting tc member auth0

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -1,12 +1,17 @@
 import { MastraAuthAuth0 } from '@mastra/auth-auth0';
 import { CompositeAuth } from '@mastra/core/server';
 
+console.log('AUTH0_DOMAIN', process.env.AUTH0_DOMAIN);
+console.log('AUTH0_AUDIENCE', process.env.AUTH0_AUDIENCE);
+console.log('AUTH0_M2M_DOMAIN', process.env.AUTH0_M2M_DOMAIN);
+console.log('AUTH0_M2M_AUDIENCE', process.env.AUTH0_M2M_AUDIENCE);
+
 export const apiAuthLayer = new CompositeAuth([
-  // TC Member Auth0 JWTs
-  new MastraAuthAuth0({
-    domain: process.env.AUTH0_DOMAIN,
-    audience: process.env.AUTH0_AUDIENCE,
-  }),
+  // // TC Member Auth0 JWTs
+  // new MastraAuthAuth0({
+  //   domain: process.env.AUTH0_DOMAIN,
+  //   audience: process.env.AUTH0_AUDIENCE,
+  // }),
   // TC M2M Auth0 JWTs
   new MastraAuthAuth0({
     domain: process.env.AUTH0_M2M_DOMAIN ?? 'topcoder-dev.auth0.com',
